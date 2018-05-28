cf6364650695c23d4567f47a117c25e38547a870
lixinthu <georgethrax@users.noreply.github.com>
2018-05-28 12:03:34 +0800

    Create plot_list.py

---
+++ plot_list.py
@@ -0,0 +1,20 @@
+def print_unicode_list(a):
+    print str(a).decode('unicode-escape')
+
+
+def print_str_list(a):
+    print str(a).decode('string-escape')
+
+    
+if __name__ == '__main__':
+	langs = [
+		'Hello, world!',
+		'你好，世界！',
+		'こんにちは世界',
+		u'Hello, world!',
+		u'你好，世界！',
+		u'こんにちは世界'
+	]
+	print(langs)
+	print_unicode_list(langs)
+	print_str_list(langs)
